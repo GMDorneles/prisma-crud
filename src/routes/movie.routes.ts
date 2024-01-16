0729c2f571edfa8bcf9ac726f7865ccb426e4f34
GABRIELLY DORNELES <gabrielly.maldonadoo@gmail.com>
2024-01-16 14:30:21 -0400

    fix: correção da rota

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { CreateMovieController } from "../modules/movies/useCases/createMovie/CreateMovieController";
+import { GetMoviesByReleaseDateController } from "../modules/movies/useCases/getMoviesByReleaseDate/GetMoviesByReleaseDateController";
+import { CreateMovieRentController } from "../modules/movies/useCases/createMovieRent/CreateMovieRentController";
 
 const createMovieController = new CreateMovieController();
-const createMovieRentController = new CreateMovieController();
+const createMovieRentController = new CreateMovieRentController();
+const getMoviesByReleaseDateController = new GetMoviesByReleaseDateController();
 
 const movieRoutes = Router();
 movieRoutes.post("/", createMovieController.handle);
 movieRoutes.post("/rent", createMovieRentController.handle);
+movieRoutes.get("/release", getMoviesByReleaseDateController.handle);
 
 export { movieRoutes };
